8672e1c2c5c81c22b147e21aed2dd57a16c7f153
Kirill Lavrentev <kirill.lavrentev@gmail.com>
2024-01-21 11:38:14 +0300

    First part done, create doubly linked list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -16,11 +16,91 @@ type ListItem struct {
 	Prev  *ListItem
 }
 
+// Define the list structure
 type list struct {
-	List // Remove me after realization.
 	// Place your code here.
+	len  int
+	head *ListItem
+	tail *ListItem
 }
 
+// Add a new node at the end of the doubly linked list
+func (dll *list) PushBack(data interface{}) *ListItem {
+	newItem := &ListItem{Value: data, Prev: nil, Next: nil}
+
+	if dll.head == nil {
+		dll.head = newItem
+		dll.tail = newItem
+	} else {
+		newItem.Prev = dll.tail
+		dll.tail.Next = newItem
+		dll.tail = newItem
+	}
+
+	dll.len++
+
+	return newItem
+}
+
+// Add a new node at the beginning of the doubly linked list
+func (dll *list) PushFront(data interface{}) *ListItem {
+	newItem := &ListItem{Value: data, Prev: nil, Next: nil}
+
+	if dll.head == nil {
+		dll.head = newItem
+		dll.tail = newItem
+	} else {
+		newItem.Next = dll.head
+		dll.head.Prev = newItem
+		dll.head = newItem
+	}
+
+	dll.len++
+	return newItem
+}
+
+// Remove element i from the doubly linked list
+func (dll *list) Remove(i *ListItem) {
+	if i == dll.head {
+		dll.head = i.Next
+	}
+	if i == dll.tail {
+		dll.tail = i.Prev
+	}
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+
+	i = nil
+
+	dll.len--
+}
+
+// Move element i to the front
+func (dll *list) MoveToFront(i *ListItem) {
+	dll.Remove(i)
+	dll.PushFront(i.Value)
+}
+
+// Length of the doubly linked list
+func (dll *list) Len() int {
+	return dll.len
+}
+
+// Get the front element
+func (dll *list) Front() *ListItem {
+	return dll.head
+}
+
+// Get the back element
+func (dll *list) Back() *ListItem {
+	return dll.tail
+}
+
+// Initialize a new empty doubly linked list
 func NewList() List {
 	return new(list)
 }
